488ee6929224570491919b77e54d88f3346d98cf
SamAxe <1377880+SamAxe@users.noreply.github.com>
2025-12-26 12:04:37 -0800

    Simple first web server with a form

--- w/bin/main.ml
+++ w/bin/main.ml
@@ -1,1 +1,42 @@
-let () = print_endline "Hello, World!"
+
+
+let show_form ?message request =
+  let open Dream_html in
+  let open HTML in
+  html []
+  [ body []
+    [ begin
+        match message with
+        | None -> p [] [ txt "(blank)" ]
+        | Some message ->
+            p [] [ txt "You entered: %s" message ]
+      end
+    ; form [ method_ `POST; action "/"]
+      [ csrf_tag request
+      ; label [ for_ "msg_id"] [ txt "Message" ]
+      ; input [ name "message"; id "msg_id"; autofocus]
+      ; input [ type_ "submit"; value "Send" ]
+      ]
+    ]
+  ]
+  |> respond
+
+
+let () =
+  Dream.run
+    ~port:8081
+    ~error_handler:Dream.debug_error_handler
+  @@ Dream.logger
+  @@ Dream.memory_sessions
+  @@ Dream.router
+    [ Dream.get  "/" show_form
+
+    ; Dream.post "/"
+        (fun request ->
+          match%lwt Dream.form request with
+          | `Ok ["message", message] -> show_form ~message request
+          | _ -> failwith "error"
+        )
+
+
+    ]
