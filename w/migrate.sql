919d44e5831499b6a6211842a7da68a8597aa9ae
SamAxe <1377880+SamAxe@users.noreply.github.com>
2025-12-27 12:19:36 -0800

    first whack at some tables

--- w/migrate.sql
+++ w/migrate.sql
@@ -6,3 +6,29 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+DROP TABLE IF EXISTS containers;
+
+CREATE TABLE containers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    author_id INTEGER
+);
+
+DROP TABLE IF EXISTS items;
+
+CREATE TABLE items (
+  id INTEGER PRIMARY KEY,
+  container_id INTEGER NOT NULL,
+  parent_id INTEGER,         -- NULL for top-level posts
+  next_id INTEGER,           -- NULL for the last item
+  content TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+  created_at INTEGER NOT NULL,
+  FOREIGN KEY(container_id) REFERENCES containers(id),
+  FOREIGN KEY(parent_id) REFERENCES items(id),
+  FOREIGN KEY(next_id) REFERENCES items(id),
+  FOREIGN KEY(author_id) REFERENCES users(id)
+);
+
